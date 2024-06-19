e8088fbdb4e760ac0cd50b1960972a826f85aa65
Claire29200 <156660483+Claire29200@users.noreply.github.com>
2024-06-19 11:22:18 +0200

    creation des 2 pages home et about et intégration des logos accueil

--- src/App.js
+++ src/App.js
@@ -1,1 +1,24 @@
+import React from "react";
+import {
+  createBrowserRouter,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* path="*" fonctionne si jamais l'url ne correspond à rien de déclaré au dessus  */}
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Logo from '../components/Logo';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et maiores pariatur nihil magni nisi beatae omnis voluptatibus similique dolorum, praesentium amet recusandae culpa perferendis ut voluptatum unde provident nesciunt eveniet saepe? Explicabo nesciunt minima, ad corporis esse eaque vel sequi inventore debitis corrupti eum modi velit necessitatibus magni illo cumque ratione atque. Itaque minus ex quo autem, incidunt vero error, repudiandae deleniti asperiores veritatis voluptatum nostrum commodi labore cupiditate blanditiis at quibusdam magnam porro veniam? Accusamus corporis quae eos numquam soluta id deserunt deleniti sint fugit, eveniet, cumque repellendus possimus iure vitae officiis, non inventore mollitia ea. Consequuntur, aspernatur iste.</p>
+            <br />
+           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi inventore, aspernatur culpa delectus accusamus, temporibus necessitatibus nostrum in at, consectetur sit sapiente laborum quae fugit eaque quia eveniet soluta aut ipsam nisi! Porro, esse. Iste molestias officiis totam veritatis ipsam. Debitis et, est officia fugit voluptates consequuntur magni molestias aspernatur, expedita, itaque laboriosam amet nesciunt impedit possimus corrupti maxime? Itaque cupiditate sed maxime, iste ex porro optio unde molestias excepturi perspiciatis maiores eum nobis facere ipsam dolores, libero suscipit quia beatae voluptatem placeat! Omnis dolorem voluptatum nisi aliquam fugiat modi deleniti rem perferendis asperiores, facilis animi soluta deserunt laboriosam cum.</p>
+        </div>
+    );
+};
+
+export default About;
