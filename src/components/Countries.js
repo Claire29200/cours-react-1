4548bc38723a4d809f4c2ebfa9154b5f98433ffb
Claire29200 <156660483+Claire29200@users.noreply.github.com>
2024-06-19 17:01:13 +0200

    cartes des pays avec population et nom et drapeaux

---
+++ src/components/Countries.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Card from "./Card";
+
+const Countries = () => {
+  const [data, setData] = useState([]);
+  const [rangeValue, setRangeValue] = useState(36);
+  const [selectedRadio, setSelectedValue] = useState[""];
+  const radios = ["Africa", "America", "Asia", "Europe", "Oceania"];
+  useEffect(() => {
+    axios
+      .get("https://restcountries.com/v3.1/all")
+      .then((res) => setData(res.data));
+    // le useEffect se joue lorsque le composant est monté
+  }, []);
+  // les crochets s'appellent des callbacks
+  return (
+    <div className="countries">
+      <ul className="radio-container">
+        <input
+          type="range"
+          min="1"
+          max="250"
+          defaultValue={rangeValue}
+          onChange={(e) => setRangeValue(e.target.value)}
+        />
+        {radios.map((continent) => (
+          <li>
+            <input
+              type="radio"
+              id={continent}
+              name="continentRadio"
+              onChange={(e) => setSelectedRadio(e.target.id)}
+            />
+            <label htmlFor={continent}>{continent}</label>
+          </li>
+        ))}
+      </ul>
+      <ul>
+        {data.slice(0, rangeValue).map((country, index) => (
+          <Card key={index} country={country} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Countries;
